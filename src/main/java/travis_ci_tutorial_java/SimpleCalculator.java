05a5b2ae0027e32d3a5da9aff6566c1cf2137d13
RajBains98 <rbains1@students.towson.edu>
2019-04-01 10:48:39 -0400

    Added a multiply method into SimpleCalculator.java. Also, Tested it.

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -10,5 +10,8 @@ public class SimpleCalculator {
 	public int divide(int a, int b) {
 		return a / b;
 	}
+	public int multiply(int a, int b) {
+		return a * b;
+	}
 }
 
--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -9,6 +9,6 @@ public class SimpleCalculatorTest {
 	@Test
 	public void testAdd() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.divide(6, 3), 2);
+		assertEquals(calc.multiply(2, 1), 2);
 	}
 }
